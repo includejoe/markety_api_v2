18d6c0797458c044afe0ba635fe4d4b8479953ee
joel ayertey <kvnqjoe@gmail.com>
2023-02-01 12:13:20 +0000

    return username on loginSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -52,7 +52,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=255, write_only=True)
+    username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=128, write_only=True)
     jwt = serializers.SerializerMethodField()
 
