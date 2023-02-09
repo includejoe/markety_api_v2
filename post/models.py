c1ebd5008e5f9325c3b3a8bfb5821089b7304e8c
joel ayertey <kvnqjoe@gmail.com>
2023-02-09 00:32:50 +0000

    * condition to is_new on post model

--- post/models.py
+++ post/models.py
@@ -6,15 +6,13 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    CONDITION_CHOICES = (("new", "new"), ("used", "used"))
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     category = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, null=True)
-    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, default=None)
+    is_new = models.BooleanField(default=True)
     image1 = models.URLField()
     image2 = models.URLField()
     image3 = models.URLField()
