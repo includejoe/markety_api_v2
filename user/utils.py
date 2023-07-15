aa975321275ccbce54f9703ae7076bc48b62f3ee
joel ayertey <kvnqjoe@gmail.com>
2023-07-15 16:46:53 +0000

    mc

--- user/utils.py
+++ user/utils.py
@@ -1,8 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 import re
-import json
-from rest_framework.renderers import JSONRenderer
 
 
 def is_email_valid(value):
@@ -37,22 +35,3 @@ def is_username_valid(value):
         True,
         "Username can only contain letters, numbers, underscores and full stops. Username can't end with a full stop either",
     )
-
-
-# junk code: to be removed
-# Custom JSON Renderer
-class UserJSONRenderer(JSONRenderer):
-    # Custom method
-    charset = "utf-8"
-
-    def render(self, data, media_type=None, renderer_context=None):
-        errors = data.get("errors", None)
-        token = data.get("token", None)
-
-        if errors is not None:
-            return super(UserJSONRenderer, self).render(data)
-
-        if token is not None and isinstance(token, bytes):
-            data["token"] = token.decode("utf-8")
-
-        return json.dumps({"user": data})
